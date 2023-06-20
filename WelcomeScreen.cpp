ce731be3b78b6ba2b024132d940f1f1be230a95d
Lin-Yanjun <20307130136@fudan.edu.cn>
2023-06-20 15:12:18 +0800

    polish welcome screen

--- WelcomeScreen.cpp
+++ WelcomeScreen.cpp
@@ -5,18 +5,41 @@ void WelcomeScreen::handleInput() {
 }
 
 void WelcomeScreen::render() {
-	// 设置欢迎文字
-	GotoXY(33, 10);
-	cout << "Welcome to FDVIM Editor!";
-	GotoXY(33, 12);
-	cout << "Author: Lin Yanjun:)";
-	GotoXY(33, 14);
+	// 欢迎文字
+	/* http://patorjk.com/software/taag/#p=display&h=1&f=3D-ASCII&t=FDVIM
+	________  ________   ___      ___  ___   _____ ______      
+	|\  _____\|\   ___ \ |\  \    /  /||\  \ |\   _ \  _   \    
+	\ \  \__/ \ \  \_|\ \\ \  \  /  / /\ \  \\ \  \\\__\ \  \   
+	\ \   __\ \ \  \ \\ \\ \  \/  / /  \ \  \\ \  \\|__| \  \  
+	\ \  \_|  \ \  \_\\ \\ \    / /    \ \  \\ \  \    \ \  \ 
+	\ \__\    \ \_______\\ \__/ /      \ \__\\ \__\    \ \__\
+		\|__|     \|_______| \|__|/        \|__| \|__|     \|__|
+	*/                                                         
+	GotoXY(20, 10);
+	cout << " ________  ________   ___      ___  ___   _____ ______      ";
+	GotoXY(20, 11);
+	cout << "|\\  _____\\|\\   ___ \\ |\\  \\    /  /||\\  \\ |\\   _ \\  _   \\    ";
+	GotoXY(20, 12);
+	cout << "\\ \\  \\__/ \\ \\  \\_|\\ \\\\ \\  \\  /  / /\\ \\  \\\\ \\  \\\\\\__\\ \\  \\   ";
+	GotoXY(20, 13);
+	cout << " \\ \\   __\\ \\ \\  \\ \\\\ \\\\ \\  \\/  / /  \\ \\  \\\\ \\  \\\\|__| \\  \\  ";
+	GotoXY(20, 14);
+	cout << "  \\ \\  \\_|  \\ \\  \\_\\\\ \\\\ \\    / /    \\ \\  \\\\ \\  \\    \\ \\  \\ ";
+	GotoXY(20, 15);
+	cout << "   \\ \\__\\    \\ \\_______\\\\ \\__/ /      \\ \\__\\\\ \\__\\    \\ \\__\\";
+	GotoXY(20, 16);
+	cout << "    \\|__|     \\|_______| \\|__|/        \\|__| \\|__|     \\|__|";
+	GotoXY(20, 18);
 	system("pause"); // 暂停
 	system("cls"); // 刷新
-	// 设置使用提示
-	GotoXY(27, 10);
-	cout << "WARNING: Please Follow README.md Instructions!";
-	GotoXY(27, 12);
+	// 使用提示
+	GotoXY(20, 10);
+	cout << "AUTHOR : Lin-Yanjun";
+	GotoXY(20, 12);
+	cout << "EMAIL  : 20307130136@fudan.edu.cn";
+	GotoXY(20, 14);
+	cout << "HINT   : Please Follow README.md Instructions!";
+	GotoXY(20, 16);
 	system("pause"); // 暂停
 	system("cls"); // 刷新
 }
